71664c46c7a9c14fa95b98879d04e83f2c665fe9
dgliwa <dgliwa@luc.edu>
2014-11-27 22:12:59 -0600

    reduce complexity of question index page

--- client/templates/questions/question.js
+++ client/templates/questions/question.js
@@ -1,16 +1,3 @@
 Template.question.helpers({
-  options: function(){
-            var id = this._id;
-            return _.map(this.options, function(option){return {option: option, question: id}; });
-           },
-  option: function() {
-    return this.option.option;
-  },
-  votes: function(){
-    return this.option.votes;
-         },
-  question_name: function(){
-          return this.question;
-            }
 });
 

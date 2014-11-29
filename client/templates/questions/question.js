019f7e8e3be388cd366f569330766dafb0de1ea8
dgliwa <dgliwa@luc.edu>
2014-11-29 09:25:34 -0600

    Remove questions from application

--- client/templates/questions/question.js
+++ client/templates/questions/question.js
@@ -1,3 +1,8 @@
 Template.question.helpers({
 });
 
+Template.question.events({
+  "click .drop-question": function(){
+    Meteor.call("deleteQuestion", this._id);
+  }
+});
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,8 @@
 Meteor.methods({
   vote: function (id, vote) {
     Questions.update({$and : [{_id: id}, {"options.option" : vote} ] }, {$inc: {"options.$.votes": 1}})
-    }
+    },
+  deleteQuestion: function(id) {
+    Questions.remove({_id: id});
+  }
 });
